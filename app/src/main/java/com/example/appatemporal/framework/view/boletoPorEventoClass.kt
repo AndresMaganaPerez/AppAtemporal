eac85737318d2045ba4f1debfe00692db5c8b510
Diego-spes <74331292+Diego-spes@users.noreply.github.com>
2022-09-28 16:20:41 -0500

    Funcion de consultar boleto

--- app/src/main/java/com/example/appatemporal/framework/view/boletoPorEventoClass.kt
+++
@@ -1,7 +0,0 @@
-package com.example.appatemporal.framework.view
-
-data class boletoPorEventoClass (
-    var boletoTitulo: String,
-    var boletoFecha: String,
-    var boletoHora: String
-)
--- app/src/main/java/com/example/appatemporal/framework/view/favoritosespectador/TarjetaGrande_favoritosespectador.kt
+++
@@ -1,4 +0,0 @@
-package com.example.appatemporal.framework.view.favoritosespectador
-data class TarjetaGrande_favoritosespectador(var nombre:String, var fecha:String, var descripcion:String )
-
-
