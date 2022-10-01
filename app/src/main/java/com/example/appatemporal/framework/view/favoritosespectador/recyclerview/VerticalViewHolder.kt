284a0b066e0231ebacc8ca49d956b4279d07d1a9
Diego-spes <74331292+Diego-spes@users.noreply.github.com>
2022-10-01 17:34:05 -0500

    creacion de reciclerview de vista de eventos

--- app/src/main/java/com/example/appatemporal/framework/view/favoritosespectador/recyclerview/VerticalViewHolder.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/favoritosespectador/recyclerview/VerticalViewHolder.kt
@@ -2,14 +2,15 @@ package com.example.appatemporal.framework.view.favoritosespectador.recyclerview
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.example.appatemporal.databinding.HomepageTarjetaEventosGrandeEspectadorBinding
+import com.example.appatemporal.databinding.ActivityCardBoletoPorEventoBinding
+
 import com.example.appatemporal.framework.view.favoritosespectador.TarjetaGrande_favoritosespectador
 
 class VerticalViewHolder(view: View): RecyclerView.ViewHolder(view) {
-    private val binding = HomepageTarjetaEventosGrandeEspectadorBinding.bind(view)
+    private val binding = ActivityCardBoletoPorEventoBinding.bind(view)
         fun bind(item: TarjetaGrande_favoritosespectador){
             binding.Fecha.text = item.fecha
-            binding.Descripcion.text = item.descripcion
+            binding.Hora.text = item.descripcion
             binding.Nombre.text=item.nombre
 
     }
