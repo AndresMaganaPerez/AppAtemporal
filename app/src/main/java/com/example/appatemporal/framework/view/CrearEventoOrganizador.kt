a638272e0b47ebfb4f3eaad5b2f80dceae064bcd
Andrés Magaña Pérez <a01067963@itesm.mx>
2022-10-03 14:30:16 -0500

    Obtención de inputs y intento de acceso a inflaters

--- app/src/main/java/com/example/appatemporal/framework/view/CrearEventoOrganizador.kt
+++ app/src/main/java/com/example/appatemporal/framework/view/CrearEventoOrganizador.kt
@@ -1,6 +1,7 @@
 package com.example.appatemporal.framework.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TimePicker
@@ -9,34 +10,61 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appatemporal.R
 import com.example.appatemporal.databinding.ActivityCrearEventoBinding
+import com.example.appatemporal.databinding.ItemArtistaFormBinding
+import com.example.appatemporal.databinding.ItemCrearFuncionBinding
 import com.example.appatemporal.domain.models.FunctionModel
+import kotlinx.android.synthetic.main.item_artista_form.view.*
 import java.time.LocalDate
 import java.util.*
+import kotlin.reflect.typeOf
 
 class CrearEventoOrganizador : AppCompatActivity(){
-    private lateinit var binding: com.example.appatemporal.databinding.ActivityCrearEventoBinding
-    private var item_artista = R.layout.item_artista_form
+    private lateinit var binding: ActivityCrearEventoBinding
+    private lateinit var bindingIArt: ItemArtistaFormBinding
+    private lateinit var bindingIFun: ItemCrearFuncionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val nombreEvento = binding.NombreEvento
+        val descripcion = binding.DescripcionEvento
+        val direccion = binding.DireccionEvento
+        val ubicacion = binding.UbicacionEvento
+        val ciudad = binding.CiudadEvento
+        val estado = binding.EstadoEvento
+        val latitud = binding.LatitudEvento
+        val longitud = binding.LongitudEvento
+        val imagen = binding.UrlImagenEvento
+        val video = binding.URLVideoEvento
+
+
         binding = ActivityCrearEventoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Time input
         OnClickTime()
 
-        removeArtFormView(R.layout.item_artista_form)
-
+        // Agregar Formulario de Artista
         binding.addArtistBtn.setOnClickListener {
+            Log.d("Add Btn Click: ", "Se presionó el botón agregar artista")
             addNewArtFormView()
         }
 
+        // Eliminar Formulario de Artista
+        removeArtFormView()
+
+        // Agregar Formulario de Función
         binding.addFunctionBtn.setOnClickListener {
             addNewFunFormView()
         }
 
-
+        // Eliminar Formulario de Función
+        removeFunFormView()
 
     }
+
+
+
 
     private fun OnClickTime() {
         val timePickerI = findViewById<TimePicker>(R.id.timePickerInicio)
@@ -58,11 +86,29 @@ class CrearEventoOrganizador : AppCompatActivity(){
 
     private fun addNewArtFormView() {
         val inflater = LayoutInflater.from(this).inflate(R.layout.item_artista_form, null)
-        binding.artistaFormLayout.addView(inflater, binding.funcionFormLayout.childCount)
+        binding.artistaFormLayout.addView(inflater, binding.artistaFormLayout.childCount)
+
     }
 
-    private fun removeArtFormView(view : View) {
+    private fun removeArtFormView() {
+        // Eliminar Formulario de Artista
+        val count = binding.artistaFormLayout.childCount
+        var item : View?
+        var artItems : Array<View?> = arrayOf()
 
+        Log.d("Prueba: ", "Test01")
+
+        for (i in 0 until count) {
+            item = binding.artistaFormLayout.getChildAt(i)
+            artItems.plus(item)
+
+
+            item.deleteArtBtn.setOnClickListener {
+                Log.d("Delete Btn Click: ", "Se presionó el botón delete artista")
+                binding.artistaFormLayout.removeView(item)
+            }
+        }
+        Log.d("Arreglo de items de Artist Form: ", artItems.toString())
     }
 
     private fun addNewFunFormView() {
@@ -71,7 +117,22 @@ class CrearEventoOrganizador : AppCompatActivity(){
     }
 
     private fun removeFunFormView() {
+        /*// Eliminar Formulario de Función
+        val count2 = binding.funcionFormLayout.childCount
+        var item2 : View?
+        var funItems : Array<View?> = arrayOf()
 
+        Log.d("Prueba: ", "Test02")
+
+        for (j in 0 until count2) {
+            item2 = binding.funcionFormLayout.getChildAt(j)
+            funItems.plus(item2)
+
+            item2.deleteFunBtn.setOnClickListener {
+                Log.d("Delete Btn Click: ", "Se presionó el botón delete artista")
+                binding.funcionFormLayout.removeView(item2)
+            }
+        }*/
     }
 
 }
